39fe3468df7f453042482548b0779b7f45793ed4
Nicolas Przybylski <nicolas.prz.cooper@gmail.com>
2023-11-22 14:20:43 +0100

    refactored encoder/decoder using new GrayscaleDataset

--- dataset_creation/GraysacleDataset.py
+++ dataset_creation/GraysacleDataset.py
@@ -2,8 +2,11 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 
 class GrayscaleDataset(Dataset):
-    def __init__(self, data, labels, width, height):
+    def __init__(self, data, labels, width, height, reshape=True, normalize=False):
         super().__init__()
+
+        self.reshape = reshape
+        self.normalize = normalize
 
         self.width = width
         self.height = height
@@ -11,20 +14,27 @@ class GrayscaleDataset(Dataset):
         self.data=np.array(data)
         self.labels=np.array(labels)
 
-        self.reshaped_data = [self.reshape_image(img) for img in data]
+        if self.reshape:
+            self.data = [self.reshape_image(img) for img in self.data]
+
+        if self.normalize:
+            self.data = [self.normalize_image(img) for img in self.data]
 
     def reshape_image(self, img):
         return img.reshape(self.width, self.height)
         
 
+    def normalize_image(self, img):
+        return img/255.0
+
     def __getitem__(self, key: int):
         assert key < self.__len__()
 
-        img = self.reshaped_data[key]
+        img = self.data[key]
         label = self.labels[key]
 
         return img, label
 
     
     def __len__(self):
-        return len(self.reshaped_data)
+        return len(self.data)
