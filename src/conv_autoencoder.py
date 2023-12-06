47b55ffc5f8de04e1b38d47cfe8310cca9256f6d
Nicolas Przybylski <nicolas.prz.cooper@gmail.com>
2023-12-06 10:32:22 +0100

    added max pooling and upscale to conv autoencoder

--- src/conv_autoencoder.py
+++ src/conv_autoencoder.py
@@ -12,7 +12,7 @@ class ConvAutoencoder(BaseAutoencoder):
             self.encoder.add_module(f"encoder_{i}", nn.Conv2d(layer_sizes[i], layer_sizes[i+1], kernel_size=kernel_size, padding=1))
             if i < len(layer_sizes) - 2:
                 self.encoder.add_module(f"encoder_relu_{i}", nn.ReLU())
-                #self.encoder.add_module(f"encoder_max_pool_{i}", nn.MaxPool2d(2, 2))
+                self.encoder.add_module(f"encoder_max_pool_{i}", nn.MaxPool2d(2, 2))
                 if batch_norm:
                     self.encoder.add_module(f"encoder_batchnorm_{i}", nn.BatchNorm2d(layer_sizes[i+1]))
 
@@ -21,7 +21,7 @@ class ConvAutoencoder(BaseAutoencoder):
             self.decoder.add_module(f"decoder_{i}", nn.Conv2d(layer_sizes[i], layer_sizes[i-1], kernel_size=kernel_size, padding=1))
             if i > 1:
                 self.decoder.add_module(f"decoder_relu_{i}", nn.ReLU())
-                #self.encoder.add_module(f"encoder_max_pool_{i}", nn.Upsample(scale_factor=2, mode='nearest'))
+                self.decoder.add_module(f"encoder_upsample_{i}", nn.Upsample(scale_factor=2, mode='nearest'))
                 if batch_norm:
                     self.decoder.add_module(f"encoder_batchnorm_{i}", nn.BatchNorm2d(layer_sizes[i-1]))
 
@@ -29,9 +29,9 @@ class ConvAutoencoder(BaseAutoencoder):
         self.decoder.add_module("decoder_sigmoid", nn.Sigmoid())
     
     def forward(self, x):
-        x = x.view(-1, 1, 128, 128)
+        x = x.view(-1, 1, self.width, self.height)
 
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         
-        return encoded, decoded
+        return encoded.squeeze(), decoded.squeeze()
