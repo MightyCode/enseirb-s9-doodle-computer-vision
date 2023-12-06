e7c2e6b5f695d1e46bd3de35851aac9ac2d7f717
MightyCode <bazinmaxence@free.fr>
2023-12-06 10:48:57 +0100

    add encoding class on layers

--- src/linear_autoencoder.py
+++ src/linear_autoencoder.py
@@ -1,10 +1,11 @@
 from .base_autoencoder import BaseAutoencoder
 
+import torch
 import torch.nn as nn
 
 class LinearAutoencoder(BaseAutoencoder):
-    def __init__(self, layer_sizes, device, width, height, classes, dropout=0., batch_norm=True):
-        super().__init__(layer_sizes, device, width, height, classes, dropout=0., batch_norm=True)
+    def __init__(self, layer_sizes, device, width, height, classes, dropout=0., batch_norm=True, encode_class=False):
+        super().__init__(layer_sizes, device, width, height, classes, dropout=0., batch_norm=True, encode_class=encode_class)
 
         for i in range(len(layer_sizes) - 1):
             self.encoder.add_module(f"encoder_{i}", nn.Linear(layer_sizes[i], layer_sizes[i+1]))
@@ -13,6 +14,9 @@ class LinearAutoencoder(BaseAutoencoder):
                 self.encoder.add_module(f"encoder_dropout_{i}", nn.Dropout(dropout))
                 if batch_norm:
                     self.encoder.add_module(f"encoder_batchnorm_{i}", nn.BatchNorm1d(layer_sizes[i+1]))
+
+        if self.encode_class:
+            layer_sizes[-1] += 1
 
         # Add decoder layers
         for i in range(len(layer_sizes)-1, 0, -1):
@@ -26,3 +30,17 @@ class LinearAutoencoder(BaseAutoencoder):
 
         self.decoder.add_module("decoder_sigmoid", nn.Sigmoid())
         
+    def add_class_to_encoded(self, encoded_features, labels):
+        return torch.cat((encoded_features, labels.unsqueeze(1)), dim=1)
+    
+    def forward(self, x, labels=None):
+        encoded = self.encoder(x)
+
+        if self.encode_class:
+            if labels is None:
+                raise Exception("Labels must be provided if encode_class is True")
+            encoded = self.add_class_to_encoded(encoded, labels)
+
+        decoded = self.decoder(encoded)
+        
+        return encoded, decoded
